ecfca8951637ab1b54fd93622864ad749b7f247f
Bethwel Kiplagat <sjemnyango9@gmail.com>
2026-01-15 11:11:31 +0000

    The Navbar should not be sticky. The Image in Home shoulb be clear

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -15,7 +15,7 @@ export function Header() {
   }
   
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="relative w-full border-b bg-background">
       <div className="container flex h-14 items-center">
         <div className="mr-4 flex items-center">
           <Link href="/" className="flex items-center gap-2">
